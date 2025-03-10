e1db42876451267061f94d9c9ee99414ae8979cb
alpaylan <alpkeles99@gmail.com>
2025-03-10 12:25:27 -0400

    fix json resume import

--- cvdl-editor/fe/src/api/fetchGist.ts
+++ cvdl-editor/fe/src/api/fetchGist.ts
@@ -1,28 +1,14 @@
 "use server";
 
 export async function fetchGist(username: string) {
-	return fetch(`https://api.github.com/users/${username}/gists?per_page=100`, {
+	
+	return fetch(`https://registry.jsonresume.org/${username}.json`, {
 		method: "GET",
 		headers: {
 			"Content-Type": "application/json",
-			Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
 		},
 	})
 		.then((response) => response.json())
-		.then((data) => {
-			// look for resume.json
-			console.log("data", data);
-			console.log(data.map((gist: any) => gist.files));
-			const resume = data.find((gist: any) => gist.files["resume.json"]);
-			if (resume) {
-				return fetch(resume.files["resume.json"].raw_url)
-					.then((response) => response.json())
-					.then((data) => {
-						console.log("data", data);
-						return data;
-					});
-			}
-		})
 		.catch((error) => {
 			console.error("Error:", error);
 		});
